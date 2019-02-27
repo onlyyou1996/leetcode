0afad8ae0fbb3b5267d6c544637b949427df08c7
onlyyou1996 <44084954+onlyyou1996@users.noreply.github.com>
2019-02-27 10:45:40 +0800

    Update leetcode-20.cpp

--- leetcode-20.cpp
+++ leetcode-20.cpp
@@ -4,12 +4,12 @@ using namespace std;
 class Solution {
 public:
 	bool isValid(string s) {
-		int l = s.size();
-		stack<char> stack1;
+		int l = s.size();		//获取长度
+		stack<char> stack1;		//定义栈
 		int i = 0;
 		for (i = 0; i < l; i++) {
-			if (s[i] == '(' | s[i] == '[' | s[i] == '{') stack1.push(s[i]);
-			else {
+			if (s[i] == '(' | s[i] == '[' | s[i] == '{') stack1.push(s[i]);		//将括号左半边压入栈
+			else {																//判断右边括号是否和左半边相对应
 				if (stack1.empty()) return false;
 				if (s[i] == ')' && stack1.top() != '(') return false;
 				if (s[i] == ']' && stack1.top() != '[') return false;
@@ -17,7 +17,7 @@ public:
 				stack1.pop();
 			}
 		}
-		if (stack1.empty())  return true;
+		if (stack1.empty())  return true;										//判断是否所有对应
 		else    return false;
 	}
 };
